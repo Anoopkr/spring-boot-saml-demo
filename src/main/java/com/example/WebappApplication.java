a725cdd3b7e3819556e35165e94c12c886b16199
Anopkr <anoopply2009@gmail.com>
2020-10-22 18:40:21 +0530

    initial commit

--- src/main/java/com/example/WebappApplication.java
+++ src/main/java/com/example/WebappApplication.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
 @SpringBootApplication
-//@EnableWebMvc
 class WebappApplication extends SpringBootServletInitializer {
 
 
